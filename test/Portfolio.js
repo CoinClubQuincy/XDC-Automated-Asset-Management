d5ca71956176a20661e3d79f457da06bba4ec31c
Quincy Jones <rqjones@asu.edu>
2024-08-18 11:08:46 -0700

    Unit test

--- test/Portfolio.js
+++ test/Portfolio.js
@@ -1,37 +1,34 @@
-// const { time, loadFixture,} = require("@nomicfoundation/hardhat-network-helpers");
-// const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-// const { expect } = require("chai");
-  
-//   before("Portfolio", function () {
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-//     async function init() {
+describe("Portfolio Ledger and Portfolio", function () {
+  let PortfolioLedger, Portfolio, portfolioLedger, portfolio, owner, addr1, addr2;
 
-//     }
-  
-//     describe("Contract Unit Test", function () {
-//       it("FuncA", async function () {
-//         const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-  
-//         expect(await lock.unlockTime()).to.equal(unlockTime);
-//       });
-  
-//       it("FuncB", async function () {
-//         const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-  
-//         expect(await lock.owner()).to.equal(owner.address);
-//       });
-//     });
-//     describe("Contract Functional Test", function () {
-//       it("FuncA", async function () {
-//         const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-  
-//         expect(await lock.unlockTime()).to.equal(unlockTime);
-//       });
-  
-//       it("FuncB", async function () {
-//         const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-  
-//         expect(await lock.owner()).to.equal(owner.address);
-//       });
-//     });
-// })
+  beforeEach(async function () {
+    PortfolioLedger = await ethers.getContractFactory("portfolioLedger");
+    Portfolio = await ethers.getContractFactory("Portfolio");
+    [owner, addr1, addr2, _] = await ethers.getSigners();
+
+    portfolioLedger = await PortfolioLedger.deploy(/* constructor arguments */);
+    await portfolioLedger.deployed();
+
+    portfolio = await Portfolio.deploy(/* constructor arguments */);
+    await portfolio.deployed();
+  });
+
+  describe("Portfolio Ledger", function () {
+    it("Should create new account", async function () {
+      await portfolioLedger.createAccount("testAccount");
+      const account = await portfolioLedger.checkAccount("testAccount", 1);
+      expect(account.exist).to.equal(true);
+    });
+  });
+
+  describe("Portfolio", function () {
+    it("Should save new marketplace", async function () {
+      await portfolio.saveMarketplace(addr1.address);
+      const savedMarketplace = await portfolio.viewSavedMarketPlaces(0);
+      expect(savedMarketplace.exist).to.equal(true);
+    });
+  });
+});
