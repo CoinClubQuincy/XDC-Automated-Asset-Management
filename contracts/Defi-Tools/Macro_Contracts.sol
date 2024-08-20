d7f5916e26e808575f365bf13193ae119f394ba3
Quincy Jones <rqjones@asu.edu>
2024-08-19 18:50:19 -0700

    added schema basic

--- contracts/Defi-Tools/Macro_Contracts.sol
+++ contracts/Defi-Tools/Macro_Contracts.sol
@@ -1,15 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 
-/// @title Futures Contract
+/// @title MACRO Contract
 /// @author R Quincy Jones
 /// @notice This is an automated futures contract
 /// @dev This contract takes in a data schema and triggers contitions based on the input data
+
+
+// {
+//     "name": "Future",
+//     "type": "MACRO",
+//     ""
+// }
 
 pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 //Smart contract
 contract MACRO {
-    string private inputSchema;
+    string public inputSchema;
     constructor(string memory _Schema) {
             inputSchema = _Schema;
     }
