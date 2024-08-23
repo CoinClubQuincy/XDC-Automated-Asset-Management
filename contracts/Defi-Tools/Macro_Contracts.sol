eda97b6e2e78458b271f6344e2bd4c4277572c20
Quincy Jones <rqjones@asu.edu>
2024-08-23 12:14:23 -0700

    Update Macro_Contracts.sol

--- contracts/Defi-Tools/Macro_Contracts.sol
+++ contracts/Defi-Tools/Macro_Contracts.sol
@@ -2,25 +2,48 @@
 
 /// @title MACRO Contract
 /// @author R Quincy Jones
-/// @notice This is an automated futures contract
+/// @notice This is a template that automated a set of conditions based on input data
 /// @dev This contract takes in a data schema and triggers contitions based on the input data
 
 pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract MACRO {
-    string[] public inputSchema;
+    bool public initialized = false;
+    uint public schemaCount = 0;
+    Schema[] public schemaHistory;
 
-    constructor(string[] memory _Schema) {
-            inputSchema = _Schema;
+    struct Schema {
+        History[] history;
     }
-    modifier Validate(){
-        require(bytes(inputSchema).length > 0,"Schema must be defined");
+    struct History{
+        string[] schema;
+    }
+
+    function initialize(string[] memory _Schema) {
+        require(initialized == false,"Contract already initialized");
+        schemaHistory[schemaCount].schema = _Schema;
+        schemaCount++;
+        initialized = true
+    }
+
+    modifier Validate(string memory _inputSchema){
+        require(bytes(_inputSchema).length > 0,"Schema must be defined");
         _;
     }
+
     function modifySchema(int _index,string memory _data) public returns(string memory){
-        inputSchema[_index] = _data;
-        return inputSchema;
+        require(_index <= schemaCount,"Index out of range");
+        schemaHistory[_index] = _data;
+        return schemaHistory[_index];
+    }
+
+    function viewAllSchemaHistory() public view returns(History[] memory){
+        return schemaHistory;
+    }
+
+    function viewCurrentSchema() public view returns(string[] memory){
+        return schemaHistory[schemaCount].schema;
     }
 } 
 
