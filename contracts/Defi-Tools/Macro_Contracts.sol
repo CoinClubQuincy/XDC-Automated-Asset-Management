3a1096edbd5daa9c39ef818efb98dd5e8d6ca9aa
Quincy Jones <rqjones@asu.edu>
2024-08-23 11:11:58 -0700

    Update Macro_Contracts.sol

--- contracts/Defi-Tools/Macro_Contracts.sol
+++ contracts/Defi-Tools/Macro_Contracts.sol
@@ -5,28 +5,21 @@
 /// @notice This is an automated futures contract
 /// @dev This contract takes in a data schema and triggers contitions based on the input data
 
-
-// {
-//     "name": "Future",
-//     "type": "MACRO",
-//     ""
-// }
-
 pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-//Smart contract
+
 contract MACRO {
-    string public inputSchema;
-    constructor(string memory _Schema) {
+    string[] public inputSchema;
+
+    constructor(string[] memory _Schema) {
             inputSchema = _Schema;
     }
-
     modifier Validate(){
-        //Validate schema
         require(bytes(inputSchema).length > 0,"Schema must be defined");
         _;
     }
-    function modifySchema() public returns(string memory){
+    function modifySchema(int _index,string memory _data) public returns(string memory){
+        inputSchema[_index] = _data;
         return inputSchema;
     }
 } 
