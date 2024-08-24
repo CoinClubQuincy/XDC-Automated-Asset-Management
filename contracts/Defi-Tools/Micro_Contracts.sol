0c48249fc8829c69d199d96fad32eae23282b203
Quincy Jones <rqjones@asu.edu>
2024-08-24 05:50:29 -0700

    Create Micro_Contracts.sol

---
+++ contracts/Defi-Tools/Micro_Contracts.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/// @title MICRO Contract
+/// @author R Quincy Jones
+/// @notice This is a template that automated a set of conditions based on ia set of rule
+/// @dev This contract takes in a data schema and triggers contitions based on the input data
+
+pragma solidity >=0.8.2 <0.9.0;
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract MICRO {}
