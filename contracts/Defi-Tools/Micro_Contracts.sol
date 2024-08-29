06640f5a35beea8d5b0e05161f33bc5c3ba1b39f
Quincy Jones <rqjones@asu.edu>
2024-08-28 20:43:48 -0700

    rename parameters as well as change returns

--- contracts/Defi-Tools/Micro_Contracts.sol
+++ contracts/Defi-Tools/Micro_Contracts.sol
@@ -14,27 +14,27 @@ contract MICRO is ManagerDeployer {
     Parameters public parameters;
     struct Parameters {
         uint256 transacted_funds_rate;
-        uint256 liquid_invoice_rate;
+        uint256 liquid_invoice_intrest_rate;
     }
-    function initialize(uint set_transacted_funds_rate, uint set_liquid_invoice_rate) public returns(bool) {
+    function initialize(uint set_transacted_funds_rate, uint set_liquid_invoice_intrest_rate) public returns(bool) {
         require(!initialized, "Already initialized");
         Parameters.transacted_funds_rate = set_transacted_funds_rate;
-        Parameters.liquid_invoice_rate = set_liquid_invoice_rate;
+        Parameters.liquid_invoice_intrest_rate = set_liquid_invoice_intrest_rate;
         initialized = true;
         singleDeployerActive = false;
         return true;
     }  
-    function view_single_parameter(string memory parameter) public view returns (bool) {
+    function view_single_parameter(string memory parameter) public view returns (Parameters) {
         if (keccak256(abi.encodePacked(parameter)) == keccak256(abi.encodePacked("transacted_funds_rate"))) { return Parameters.transacted_funds_rate; }
-        if (keccak256(abi.encodePacked(parameter)) == keccak256(abi.encodePacked("liquid_invoice_rate"))) { return Parameters.liquid_invoice_rate; }
+        if (keccak256(abi.encodePacked(parameter)) == keccak256(abi.encodePacked("liquid_invoice_intrest_rate"))) { return Parameters.liquid_invoice_intrest_rate; }
         else { revert("Parameter not found"); }
     }
     function view_all_parameters() public view returns (Parameters memory) {
         return Parameters;
     }
-    function set_single_parameter(string memory parameter, uint256 value) public {
+    function set_single_parameter(string memory parameter, uint256 value) public returns(Parameters) {
         if (keccak256(abi.encodePacked(parameter)) == keccak256(abi.encodePacked("transacted_funds_rate"))) { Parameters.transacted_funds_rate = value; }
-        if (keccak256(abi.encodePacked(parameter)) == keccak256(abi.encodePacked("liquid_invoice_rate"))) { Parameters.liquid_invoice_rate = value; }
+        if (keccak256(abi.encodePacked(parameter)) == keccak256(abi.encodePacked("liquid_invoice_intrest_rate"))) { Parameters.liquid_invoice_intrest_rate = value; }
         else { revert("Parameter not found"); }
     }
 }
